2c2c20120997bdcce4869d41df5a69d2bec5e316
Nidhi Shetty <nidhishettyy05@gmail.com>
2026-01-29 15:54:32 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+import string
+import random
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+print(generate_password(16))
